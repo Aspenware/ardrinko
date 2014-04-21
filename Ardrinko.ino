b3326bcc732e81c63b6ea31665cfbdd706c03929
Drew DeVault <sir@cmpwn.com>
2014-04-21 15:45:14 -0600

    Send door status via UDP

--- Ardrinko.ino
+++ Ardrinko.ino
@@ -5,8 +5,8 @@
 
 // Pin layout:
 //  A0: Temperature reading (from TMP36 probe)
+//  A1: Photo resistor (SEN-09088)
 
-const int temperaturePin = 0;
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xB8, 0x93 };
 IPAddress ip(10, 5, 0, 10);
 IPAddress remote_ip(10, 5, 0, 1);
@@ -20,25 +20,29 @@ void setup() {
 
 void loop() {
   int32_t temp = getTemp();
-  Serial.print("Temp: ");
-  Serial.print(temp);
-  Serial.print(" degrees Celcius\n");
+  int32_t door = getDoor();
   Udp.beginPacket(remote_ip, 54369);
-  //Udp.write("Hello from arduino!");
-  uint8_t *buf = (uint8_t*)&temp;
-  Udp.write(buf, sizeof(int32_t));
-  Udp.write(buf, sizeof(int32_t));
-  Udp.write(buf, sizeof(int32_t));
-  Udp.write(buf, sizeof(int32_t));
+  Udp.write((uint8_t*)&temp, sizeof(int32_t));
+  Udp.write((uint8_t*)&temp, sizeof(int32_t));
+  Udp.write((uint8_t*)&temp, sizeof(int32_t));
+  Udp.write((uint8_t*)&temp, sizeof(int32_t));
+  Udp.write((uint8_t*)&door, sizeof(int32_t));
   Udp.endPacket();
-  delay(1000);
+  delay(100);
+}
+
+float readVoltage(int pin) {
+  return analogRead(pin) * (5.0 / 1023.0);
 }
 
 int32_t getTemp() {
-  int raw = analogRead(A0);
-  float voltage = raw * (5.0 / 1023.0);
+  float voltage = readVoltage(A0);
   // 0.75V at 25 deg Celcius, 0.01V change per deg Celcius
-  float temp = (voltage - 0.5) * 100;
+  float temp = (100 * voltage) - 75;
   return (int32_t)(temp * 100);
+}
+
+int32_t getDoor() {
+  return (int32_t)analogRead(A1) > 700;
 }
 
